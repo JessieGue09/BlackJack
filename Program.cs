d4b9dd56f31198960b9ac6bdd8a52651e95b590a
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-25 20:14:59 -0700

    Agregar la funcion preguntar al jugador si desea alguna carta. Mostrar la opción seleccionada.

--- Program.cs
+++ Program.cs
@@ -9,13 +9,37 @@ namespace BlackJack
         /// </summary>
         static void PrimerJuego()
         {
-            // TODO: El jugador comienza con un manojo de cartas vacio
+            // Mostrar al jugar con cuantas cartas comienza
             Console.WriteLine("¡Bienvenido!, has comenzado con 0 cartas.");
 
         }
+
+        /// <summary>
+        /// Preguntar al usuario si desea un carta.
+        /// </summary>
+        /// <returns>La decision tomada: Y - Desea cartas, N - no desea cartas</returns>
+        static string SolicitarCartas()
+        {
+            // Y: Yes
+            // N: No
+            Console.WriteLine("¿Deseas tomar una carta?");
+            Console.WriteLine("Y = Deseo una carta");
+            Console.WriteLine("N = No deseo una carta");
+
+             string option = Convert.ToString(Console.ReadLine());
+             return option;
+        }
+
         static void Main(string[] args)
         {
+           // TODO: El jugador comienza con un manojo de cartas vacio
             PrimerJuego();
+
+            // TODO: Preguntar al jugar si desea quedarse con las cartas que tiene, o si solicita una carta.
+            string option = SolicitarCartas();
+            Console.WriteLine("Opción seleccionada: " + option);
+
+
             //CartasJugador();
         }
     }
